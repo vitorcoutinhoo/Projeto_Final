9abadd45e41f684cf24bd670a7e3a5dfe4aa61a9
Vitor <vitorcooutinho@gmail.com>
2025-02-19 16:07:41 -0300

    feat: get vrx and vry values

--- final.c
+++ final.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
+#define vrx 27
+#define vry 26
 
-
-int main()
-{
+int main() {
     stdio_init_all();
 
+    adc_init();
+    adc_gpio_init(vrx);
+    adc_gpio_init(vry);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        adc_select_input(1);
+        uint16_t vrx_value = adc_read();
+
+        adc_select_input(0);
+        uint16_t vry_value = adc_read();
+
+        printf("vrx: %d | vry: %d\n", vrx_value, vry_value);
+        sleep_ms(10);
     }
 }
